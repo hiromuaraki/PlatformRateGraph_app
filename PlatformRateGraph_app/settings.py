6e7ff27603dee4d2d0adc1dddfa41e31e159650f
hiromuaraki <shinji06011@gmail.com>
2025-12-11 11:40:40 +0900

    DEBUGをFalseへ変更

--- PlatformRateGraph_app/settings.py
+++ PlatformRateGraph_app/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True # 開発時
+DEBUG = False  # 本番時
 
 ALLOWED_HOSTS = ["localhost", ".onrender.com"]
 
